895dd4af0bfec0e08c7bf4a7be58ff5afc5c9471
mikethecanuck <mikethecanuck@gmail.com>
2017-09-18 11:03:04 -0700

    cleanup of class structure to more closely mirror the React Tutorial structures and improve my ability to reason about the layout by removing redundant layers

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  renderEncounter(i) {
+    return <Encounter value={i} />;
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Timeline</h2>
+          <h1>Timeline</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Timeline">
+          {this.renderEncounter(0)}
+          {this.renderEncounter(1)}
+          {this.renderEncounter(2)}
+        </div>
       </div>
     );
   }
@@ -18,43 +24,25 @@ class App extends Component {
 
 export default App;
 
-// TBD this might be redundant with the App class
-class Timeline extends Component {
-  render() {
-    return (
-      <div classname="Timeline">
-
-      </div>
-    );
-  }
-}
-
 // This is the repeating element that displays one or more objects in the Timeline, one per encounter
 class Encounter extends Component {
   render() {
     return (
-      <div classname="Encounter">
-        <div classname="Person-image">
+      <div className="Encounter">
+        <div className="Person-image">
           {/* TODO: insert appropriately parameterized <img> tag */}
         </div>
-        <div classname="Encounter-data">
-        {/* TBD this might be redundant with the Encounter-data-area class */}
+        <div className="Encounter_data">
+          <div className="Encounter-first-row">
+            <div className="Person-name"></div>
+            <div className="Encounter-date"></div>
+          </div>
+          <div className="Encounter-second-row">
+            <div className="Encounter-event"></div>
+          </div>
         </div>
-        <div classname="Encounter-navigation"></div>
+        <div className="Encounter-navigation"></div>
         <hr />
-      </div>
-    );
-  }
-}
-
-// This element nests inside the "Encounter-data" div of the Encounter class
-class EncounterData extends Component {
-  render() {
-    return (
-      <div classname="Encounter-data-area">
-        <div classname="Person-name"></div>
-        <div classname="Encounter-date"></div>
-        <div classname="Encounter-event"></div>
       </div>
     );
   }
