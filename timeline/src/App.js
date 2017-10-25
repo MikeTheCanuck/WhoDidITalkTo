b46c51fe25963304270f3d8340566dc6dd07c9bc
Mike Lonergan <mikethecanuck@gmail.com>
2017-10-25 10:27:41 -0700

    Have successfully passed data down to EncounterList component from App via the tutorial https://www.fullstackreact.com/30-days-of-react/day-5/

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -11,7 +11,7 @@ import firebase from 'firebase';
 const activities = [
   {
     timestamp: new Date().getTime(),
-    text: "Ate lunch",
+    text: "Ate trashy lunch",
     user: {
       id: 1, name: 'Nate',
       avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
@@ -29,6 +29,39 @@ const activities = [
   },
 ]
 
+// TODO: pull in remote data
+// This currently pulls in a local array of data
+const encounters = [
+  {
+    Date: "2017-08-17",
+    Person: "Donna Troy",
+    Event: "PDXWIT Summer Soiree",
+    Location: "",
+    Topics: "Zapproved current internship program",
+    Photo: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+  },
+  {
+    Date: "2017-08-17",
+    Person: "Bobbie Morse",
+    Event: "PDXWIT Summer Soiree",
+    Location: "",
+    Topics: "Going on vacation"
+  },
+  {
+    Date: "2017-08-31",
+    Person: "Pepper Potts",
+    Event: "Consciousness Hacking meetup",
+    Location: "",
+    Topics: ""
+  },
+  {
+    Date: "2017-08-06",
+    Person: "Benjamin Grimm",
+    Event: "High Church of Lean Coffee",
+    Location: "",
+    Topics: "we've met multiple times, had extended conversations"
+  }
+]
 
 class App extends Component {
   constructor() {
@@ -43,103 +76,12 @@ class App extends Component {
     };
     firebase.initializeApp(config);
     super();
-
-    // TODO: pull in remote data
-    // This currently pulls in a local array of data
-    this.state = {
-      encounters: [
-        {
-          "Date": "2017-08-17",
-          "Person": "Donna Troy",
-          "Event": "PDXWIT Summer Soiree",
-          "Location": "",
-          "Topics": "Zapproved current internship program"
-        },
-        {
-          "Date": "2017-08-17",
-          "Person": "Bobbie Morse",
-          "Event": "PDXWIT Summer Soiree",
-          "Location": "",
-          "Topics": "Going on vacation"
-        },
-        {
-          "Date": "2017-08-31",
-          "Person": "Pepper Potts",
-          "Event": "Consciousness Hacking meetup",
-          "Location": "",
-          "Topics": ""
-        },
-        {
-          "Date": "2017-08-31",
-          "Person": "Zatanna",
-          "Event": "Consciousness Hacking meetup",
-          "Location": "",
-          "Topics": "Mark Goldby (7 years as a patient), crystal in shell casing"
-        },
-        {
-          "Date": "2017-09-01",
-          "Person": "Pepper Potts",
-          "Event": "AgilePDX Pub Lunch",
-          "Location": "",
-          "Topics": "Alistair Cockburn poetry"
-        },
-        {
-          "Date": "2017-09-01",
-          "Person": "Tony Stark",
-          "Event": "AgilePDX Pub Lunch",
-          "Location": "",
-          "Topics": "Working as Product Owner, Digital Strategist in an Agency"
-        },
-        {
-          "Date": "2017-09-02",
-          "Person": "Volstagg",
-          "Event": "",
-          "Location": "Ground Breaker Gastropub",
-          "Topics": "Baby shower, baby boy arriving January 2018"
-        },
-        {
-          "Date": "2017-08-29",
-          "Person": "Jean Grey",
-          "Event": "",
-          "Location": "Elevator Commons",
-          "Topics": "UX meetups, design career, current employer actively discouraging above-and-beyond"
-        },
-        {
-          "Date": "2017-08-17",
-          "Person": "Jean Grey",
-          "Event": "PDXWIT Summer Soiree",
-          "Location": "",
-          "Topics": "side hustles - wedding site, wedding professionals consulting, career coaching"
-        },
-        {
-          "Date": "2017-08-24",
-          "Person": "Jean-Paul Beaubier",
-          "Event": "ProductTank PDX",
-          "Location": "",
-          "Topics": "leaving Digimarc, getting divorced, going to travel"
-        },
-        {
-          "Date": "2017-08-24",
-          "Person": "Felicia Hardy",
-          "Event": "ProductTank PDX",
-          "Location": "",
-          "Topics": "leaving comScore, happy to give me background on company"
-        },
-        {
-          "Date": "2017-08-06",
-          "Person": "Benjamin Grimm",
-          "Event": "High Church of Lean Coffee",
-          "Location": "",
-          "Topics": "we've met multiple times, had extended conversations"
-        }
-      ]
     }
-  }
 
   renderEncounter(i) {
-    return <Encounter valuePerson={this.state.encounters[i].Person} 
-                      valueDate={this.state.encounters[i].Date}
-                      valueEvent={this.state.encounters[i].Event} />;
+    return <Encounter valuePerson={encounters[i].Person} 
+                      valueDate={encounters[i].Date}
+                      valueEvent={encounters[i].Event} />;
   }
 
   render() {
@@ -149,21 +91,21 @@ class App extends Component {
           <h1>Timeline</h1>
         </div>
         <div className="Timeline">
-          <Content activities={activities} />
-          <EncounterList db={firebase} />
-          <MessageList db={firebase} />
-          {/* {this.renderEncounter(0)}
+
+          {/* <Content activities={activities} /> */}
+
+          <EncounterList encounters={encounters}
+                         db={firebase} />
+
+          {/* 
+          <MessageList db={firebase} /> 
+          */}
+          {/* 
+          {this.renderEncounter(0)}
           {this.renderEncounter(1)}
           {this.renderEncounter(2)}
           {this.renderEncounter(3)}
-          {this.renderEncounter(4)}
-          {this.renderEncounter(5)}
-          {this.renderEncounter(6)}
-          {this.renderEncounter(7)}
-          {this.renderEncounter(8)}
-          {this.renderEncounter(9)}
-          {this.renderEncounter(10)}
-          {this.renderEncounter(11)} */}
+           */}
         </div>
       </div>
     );
--- timeline/src/components/EncounterList.js
+++ timeline/src/components/EncounterList.js
@@ -5,47 +5,82 @@ import Encounter from './Encounter';
 import _ from 'lodash';
 
 class EncounterList extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      encounters: []
-    };
-    let encounterApp = this.props.db.database().ref('testencounters');
-    encounterApp.on('value', snapshot => {
-      this.getData(snapshot.val());
-    });
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     encounters: []
+  //   };
+  //   let encounterApp = this.props.db.database().ref('testencounters');
+  //   encounterApp.on('value', snapshot => {
+  //     this.getData(snapshot.val());
+  //   });
+  // }
 
-  getData(values){
-    let encountersVal = values;
-    let encounters = _(encountersVal)
-                      .keys()
-                      .map(encounterKey => {
-                          let cloned = _.clone(encountersVal[encounterKey]);
-                          cloned.key = encounterKey;
-                          return cloned;
-                      })
-                      .value();
-      this.setState({
-        encounters: encounters
-      });
-  }
+  // getData(values){
+  //   let encountersVal = values;
+  //   let encounters = _(encountersVal)
+  //                     .keys()
+  //                     .map(encounterKey => {
+  //                         let cloned = _.clone(encountersVal[encounterKey]);
+  //                         cloned.key = encounterKey;
+  //                         return cloned;
+  //                     })
+  //                     .value();
+  //     this.setState({
+  //       encounters: encounters
+  //     });
+  // }
 
   render() {
-    let encounterNodes = this.state.encounters.map((encounter) => {
-      return (
-        <div className="card" id="encounter">
-          <div className="card-content">
-            <Encounter encounter = {encounter.encounter} />
-          </div>
-        </div>
-      )
-    });
+    const {encounters} = this.props; //ES6 destructuring, whatever that means
+
     return (
-      <div>
-        {encounterNodes}
-      </div>
-    );
+      <div className="encounters">
+        <div classname="line"></div>
+
+      {/* Timeline item */}
+      {encounters.map((encounter) => {
+        return (
+          <div className="Encounter">
+          <div className="Person-image">
+            <img src={encounter.Photo} />
+          </div>
+          <div className="Encounter_data">
+            <div className="Encounter-first-row">
+              <div className="Person-name">
+                Who: {encounter.Person}
+              </div>
+              <div className="Encounter-date">
+                When: {encounter.Date}
+              </div>
+            </div>
+            <div className="Encounter-second-row">
+              <div className="Encounter-event">
+                Where: {encounter.Event}
+              </div>
+            </div>
+          </div>
+          <hr />
+        </div>            
+        )
+      }
+      )}
+      </div>            
+    )
+    // let encounterNodes = encounters.map((encounter) => {
+    //   return (
+    //     <div className="card">
+    //       <div className="card-content">
+    //         <Encounter encounter = {encounter.encounter} />
+    //       </div>
+    //     </div>
+    //   )
+    // });
+    // return (
+    //   <div>
+    //     {encounterNodes}
+    //   </div>
+    // );
   }
 }
 
