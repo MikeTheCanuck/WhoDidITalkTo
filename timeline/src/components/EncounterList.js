81ff6b5a6cda1bdf26f4a6c1b0f50e0e49ba396e
Mike Lonergan <mikethecanuck@gmail.com>
2017-11-03 14:55:29 -0700

    cleanup comments and better notes to self in advance of possible code review tomorrow

--- timeline/src/components/EncounterList.js
+++ timeline/src/components/EncounterList.js
@@ -1,24 +1,30 @@
 // EncounterList.js
 
+// TODO: refactor encounterz to the App component so it can be shared among all pages
+
 import React, {Component} from 'react';
 import Encounter from './Encounter';
 // import _ from 'lodash';
 
-class EncounterList extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     encounters: []
-  //   };
-  //   let encountersRef = this.props.db.database().ref('testencounters');
-  //   encountersRef.on('value', snapshot => {
-  //     this.getData(snapshot.val());
+  class EncounterList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    encounterz: []
+    }
+
+  // const encountersRef = this.props.db.database().ref('testencounters');
+  let encountersRef = this.props.db.database().ref('testencounters');    
+
+  // ATTEMPT #1
+  //
+  // encountersRef.on('value', snapshot => {
+  //   this.getData(snapshot.val());
   //   });
   // }
-
   // getData(values){
   //   let encountersVal = values;
-  //   let encounters = _(encountersVal)
+  //   let encounterz = _(encountersVal)
   //                     .keys()
   //                     .map(encounterKey => {
   //                         let cloned = _.clone(encountersVal[encounterKey]);
@@ -27,51 +33,40 @@ class EncounterList extends Component {
   //                     })
   //                     .value();
   //     this.setState({
-  //       encounters: encounters
+  //       encounterz: encounterz
   //     });
   // }
 
-  constructor(props){
-    super(props);
-    this.state = {
-    encounterz: []
-    }
+  // ATTEMPT #2
+  //
+  // let _this = this;
+  // encountersRef.on('value', function(snapshot) {
+  //   console.log(snapshot.val());
 
-    // let encountersRef = this.props.db.database().ref('testencounters');
-    const encountersRef = this.props.db.database().ref('testencounters');
-    
-    // let _this = this;
-    // encountersRef.on('value', function(snapshot) {
-    //   console.log(snapshot.val());
+  //   _this.setState({
+  //     encounterz: snapshot.val(),
+  //     loading: false
+  //   });
+  // });
 
-    //   _this.setState({
-    //     encounterz: snapshot.val(),
-    //     loading: false
-    //   });
-    // });
-
-    // this pattern copied from https://css-tricks.com/intro-firebase-react/
-    encountersRef.on('value', (snapshot) => {
-      let encounterz = snapshot.val();
-      let newState = [];
-      for (let encounter in encounterz) {
-        newState.push({
-          id: encounter,
-          Person: encounterz[encounter].Name,
-          Date: encounterz[encounter].Date,
-          Event: encounterz[encounter].Event
-        });
-      }
-      this.setState({
-        encounterz: newState
+  // ATTEMPT #3
+  // this pattern copied from https://css-tricks.com/intro-firebase-react/
+  encountersRef.on('value', (snapshot) => {
+    let encounterz = snapshot.val();
+    let newState = [];
+    for (let encounter in encounterz) {
+      newState.push({
+        id: encounter,
+        Person: encounterz[encounter].Name,
+        Date: encounterz[encounter].Date,
+        Event: encounterz[encounter].Event
       });
+    }
+    this.setState({
+      encounterz: newState
     });
+  });
   } // results in a list of encounterz objects pulled from the 'testencounters' firebase reference
-
-
-  // TODO: convert encounterz from list to Array
-  // TODO: send encounterz Array members as state to the Encounter component
-  // TODO: refactor encounterz to the App component so it can be shared among all pages
 
   render() {
     const {encounters} = this.props; //ES6 destructuring aka replacing const encounters = this.props.encounters;
