2f82ef3410758cfcd8d2d268c311451b65d26d55
Mike Lonergan <mikethecanuck@gmail.com>
2017-11-14 18:31:46 -0800

    Swapped 'table' in firebase to observe interaction with existing data

--- src/components/NewEncounter.js
+++ src/components/NewEncounter.js
@@ -29,7 +29,7 @@ class NewEncounter extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const itemsRef = this.props.db.database().ref('items');
+    const itemsRef = this.props.db.database().ref('testencounters');
     // record to be pushed has key-value pairs of "name of firebase field": "value of that field"
     const item = {
       Person: this.state.fullname,
