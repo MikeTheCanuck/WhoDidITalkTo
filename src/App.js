52aada513562c2d2e72674aff5a859bdf8e26d95
Mike Lonergan <mikethecanuck@gmail.com>
2017-11-14 17:40:22 -0800

    NewEncounter component - refactoring code out of App.js - removed redundant code from App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import API from './util/api';
+import firebase from './firebase-config';
 import EncounterList from './components/EncounterList';
 import NewEncounter from './components/NewEncounter';
-import firebase from './firebase-config';
+import API from './util/api';
 
 class App extends Component {
   state = {
     encounters: [],
-    fullname: '',
   };
-
-/* TODO: research why initializing state variables doesn't have to be in the constructor(), and if the same 
-   trick works for binding this in the constructor */
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
   /* Kyle advises this stage of the component lifecycle is preferable to the constructor stage, 
      to pull data from the data layer, as it enables the app to display skeletal UI while 
@@ -35,28 +26,6 @@ class App extends Component {
         }
       );
   };
-
-  handleChange(e) {
-    /* Takes the named control e.g. name="fullname"
-       and sets the state variable with the same name
-       to the value of the input submitted so far by the time this event fires */
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    // record to be pushed has key-value pairs of "name of firebase field": "value of that field"
-    const item = {
-      Name: this.state.fullname
-    }
-    itemsRef.push(item);
-    this.setState({
-      fullname: ''
-    })
-  }
 
   render() {
     return (
