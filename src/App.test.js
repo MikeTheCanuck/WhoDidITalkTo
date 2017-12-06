cb4e1099f5d085ece61ba0cd63fa54bda6870c53
Mike Lonergan <mikethecanuck@gmail.com>
2017-12-05 17:04:03 -0800

    Smoke tests added to two Components per Facebook guidance

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('should render its children', () => {
+    const children = (<h1>Test</h1>);
+    const wrapper = shallow(
+      <App>
+        {children}
+      </App>,
+    );
+    expect(wrapper.contains(children)).to.eql(true);
+  });
 });
---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+// Global setup file for testing
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
