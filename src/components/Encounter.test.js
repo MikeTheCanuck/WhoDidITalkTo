6f2f52c65d9b9bb3aaf62abbb371935dae492a18
Mike Lonergan <mikethecanuck@gmail.com>
2017-12-05 18:03:24 -0800

    Adding stub test and commented non-functioning tests to give myself a place to start

---
+++ src/components/Encounter.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Encounter from './Encounter';
+
+describe('<Encounter />', () => {
+  it('does not have functioning tests...', () => {
+    expect(true).toEqual(true);
+  });
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<Encounter />, div);
+//   });
+});
