e02b156110f1b70351ed205c5fbb6c80531f58c8
Mike Lonergan <mikethecanuck@gmail.com>
2017-12-05 18:02:31 -0800

    corrected for methods not available in current dependencies

--- src/components/NewEncounter.test.js
+++ src/components/NewEncounter.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NewEncounter from './NewEncounter';
 
-describe('<Encounter />', () => {
+describe('<NewEncounter />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<NewEncounter />, div);
